edfdde8ca9828499be90c5854b496c882e9da1e7
yanderjromerol-arch <yanderj.romerol@autonoma.edu.co>
2026-04-25 16:30:29 -0500

    se soluciono errores de escritutuca se le agrego lineas de codigo

--- Reserva.java
+++ Reserva.java
@@ -1,29 +1,64 @@
-public class Reserva{
+public class Reserva {
 
-    int id;
-    String usuario;
-    String vuelo;
-    String hotel;
+    private int id;
+    private String usuario;
+    private String vuelo;
+    private String hotel;
 
-    void crear(int id, String usuario, String vuelo, String hotel) {
+   
+    public Reserva(int id, String usuario, String vuelo, String hotel) {
         this.id = id;
         this.usuario = usuario;
         this.vuelo = vuelo;
         this.hotel = hotel;
     }
 
-        public void mostrarReserva() {
+  
+    public void mostrarReserva() {
         System.out.println("ID: " + id);
         System.out.println("Usuario: " + usuario);
         System.out.println("Vuelo: " + vuelo);
         System.out.println("Hotel: " + hotel);
     }
-     public void imprimirResumen() {
+
+    
+    public void imprimirResumen() {
         System.out.println("\n============================");
         System.out.println("      DETALLE DE RESERVA     ");
         System.out.println("============================");
-
-
+        System.out.println("ID: " + id);
+        System.out.println("Cliente: " + usuario);
+        System.out.println("Vuelo reservado: " + vuelo);
+        System.out.println("Hotel reservado: " + hotel);
+        System.out.println("============================\n");
     }
 
+    
+    public int getId() {
+        return id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getVuelo() {
+        return vuelo;
+    }
+
+    public String getHotel() {
+        return hotel;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public void setVuelo(String vuelo) {
+        this.vuelo = vuelo;
+    }
+
+    public void setHotel(String hotel) {
+        this.hotel = hotel;
+    }
 }
